5b8eca810566dbb0e9edd9bd6eb9f0bde2c7cc7e
taroosg <root@taroosg.dev>
2021-02-07 15:41:54 +0900

    update enemy number

--- poke/src/main.rs
+++ poke/src/main.rs
@@ -20,7 +20,7 @@ impl Pokemon {
   }
 
   fn attack(&self, target: &mut Pokemon) {
-    println!("{}の攻撃！！", self.name);
+    println!("{} の こうげき！", self.name);
     target.damage(self.power, target.defence)
   }
 
@@ -33,59 +33,61 @@ impl Pokemon {
       let adjust_coefficient = create_adjustment_random();
       damage = (param as f64 * adjust_coefficient).round() as u32;
     }
-    println!("{}に{}のダメージ！！", self.name, damage);
+    println!("{} に {} の ダメージ！", self.name, damage);
     self.hp -= damage as isize;
-    println!("{}のHP：{}\n", self.name, self.hp);
+    println!("{} の HP：{}\n", self.name, self.hp);
 
     if self.hp <= 0 {
-      println!("{}は倒れた！\n", self.name);
+      println!("{} は たおれた！\n", self.name);
       self.dead = true;
     }
   }
 }
 
 fn main() {
-  let mut hero = Pokemon::new("Polygon".to_string(), 500, 100, 75);
+  let mut my_pokemon = Pokemon::new("Polygon".to_string(), 500, 100, 75);
 
   let mut enemy = Vec::<Pokemon>::new();
-  // 敵を3体生成する
-  for i in 0..3 {
+  // 敵を1-3体生成する
+  let enemy_number = rand::thread_rng().gen_range(1, 4);
+  for i in 0..enemy_number {
+    let adjust_coefficient = create_adjustment_random();
     enemy.push(Pokemon::new(
       "敵".to_string() + &(i + 1).to_string(),
-      100,
-      100,
-      50,
+      (100 as f64 * adjust_coefficient).round() as isize,
+      (100 as f64 * adjust_coefficient).round() as usize,
+      (50 as f64 * adjust_coefficient).round() as usize,
     ))
   }
 
   loop {
-    hero.attack(&mut enemy[0]);
+    my_pokemon.attack(&mut enemy[0]);
 
     // 敵の死を確認
     if enemy[0].dead {
       enemy.remove(0);
     }
 
-    if judge(&hero, enemy.len()) {
+    if judge(&my_pokemon, enemy.len()) {
       break;
     }
 
     for e in &enemy {
-      e.attack(&mut hero);
+      e.attack(&mut my_pokemon);
     }
 
-    if judge(&hero, enemy.len()) {
+    if judge(&my_pokemon, enemy.len()) {
       break;
     }
   }
 }
 
-fn judge(hero: &Pokemon, enemy_count: usize) -> bool {
-  if hero.dead {
-    println!("{}の敗北！！", hero.name);
+fn judge(my_pokemon: &Pokemon, enemy_count: usize) -> bool {
+  if my_pokemon.dead {
+    println!("{} は めのまえが まっくらに なった！", my_pokemon.name);
     true
   } else if enemy_count == 0 {
-    println!("{}の勝利！！", hero.name);
+    println!("{} は しょうぶに かった！", my_pokemon.name);
     true
   } else {
     false
@@ -94,6 +96,5 @@ fn judge(hero: &Pokemon, enemy_count: usize) -> bool {
 
 // 調整用：0.9から1.1で乱数
 fn create_adjustment_random() -> f64 {
-  let mut rng = rand::thread_rng();
-  rng.gen_range(0.9f64, 1.1f64)
+  rand::thread_rng().gen_range(0.9f64, 1.1f64)
 }
