1fbf1eaaad7d7aa29a3488c4b3608fd82f977aca
taroosg <root@taroosg.dev>
2021-04-03 00:10:27 +0900

    update algorithm add recursive_digits_added

--- algorithm/src/main.rs
+++ algorithm/src/main.rs
@@ -1,11 +1,15 @@
 fn main() {
   println!("Input One Line!");
-  let player_input: isize = {
+  let player_input: u32 = {
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).unwrap();
     s.trim_end().to_string().parse().unwrap()
   };
-  println!("The {}th number is {:?}.", player_input, f(player_input));
+  println!(
+    "Input is {}, number is {:?}.",
+    player_input,
+    recursive_digits_added(player_input)
+  );
 }
 
 // // 豆腐の一辺から表面積を求める関数
@@ -56,20 +60,43 @@ fn f(n: isize) -> isize {
   now
 }
 
+// 数字の分割
+fn recursive_digits_added(n: u32) -> u32 {
+  let mut result_array = vec![];
+  fn a(num: u32, result_array: &mut Vec<u32>) {
+    let num_array: Vec<u32> = num
+      .to_string()
+      .chars()
+      .map(|c| c.to_digit(10).unwrap())
+      .collect();
+    let array_sum: u32 = num_array.iter().sum();
+    result_array.insert(0, array_sum);
+    match num_array.len() {
+      1 => return,
+      _ => a(array_sum, result_array),
+    }
+  }
+  a(n, &mut result_array);
+  match result_array.len() {
+    1 => result_array.iter().sum(),
+    _ => result_array.iter().skip(1).sum(),
+  }
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
   #[test]
   fn it_works() {
-    assert_eq!(f(1), 1);
-    assert_eq!(f(2), 1);
-    assert_eq!(f(3), 2);
-    assert_eq!(f(4), 3);
-    assert_eq!(f(5), 5);
-    assert_eq!(f(6), 8);
-    assert_eq!(f(7), 13);
-    assert_eq!(f(8), 21);
-    assert_eq!(f(9), 34);
+    assert_eq!(recursive_digits_added(5), 5);
+    assert_eq!(recursive_digits_added(8), 8);
+    assert_eq!(recursive_digits_added(12), 3);
+    assert_eq!(recursive_digits_added(98), 25);
+    assert_eq!(recursive_digits_added(3528), 27);
+    // assert_eq!(recursive_digits_added(99999999999884), 132);
+    assert_eq!(recursive_digits_added(5462), 25);
+    assert_eq!(recursive_digits_added(45622943), 43);
+    assert_eq!(recursive_digits_added(9514599), 48);
   }
 }
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233,
